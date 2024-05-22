31f8aa64c2f86dc7049decaf9b6db550f4dd83e6
kemalbekcan <kemalbekcan@gmail.com>
2024-05-22 14:10:18 +0300

    initial commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,8 @@ import Chat from "@/components/Sidebar/Chat";
 import Navigation from "@/components/Navigation/Navigation";
 import Products from "@/components/Products/Products";
 import FavouriteActions from "@/components/Products/FavouriteActions";
-import BasicButton from "@/components/UI/BasicButton";
-import Select from "@/components/UI/Select";
+import BasicButton from "@/components/Ui/BasicButton";
+import Select from "@/components/Ui/Select";
 import Loading from "@/components/Loading/Loading";
 import Error from "@/components/Error/Error";
 
