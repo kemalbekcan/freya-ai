223b95a0bbf1e2d185ef3a3f15c10cec628ba9cf
kemalbekcan <kemalbekcan@gmail.com>
2024-05-22 14:06:28 +0300

    initial commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,12 @@
-import Sidebar from "./Sidebar/Sidebar";
-import Chat from "./Sidebar/Chat";
-import Navigation from "./Navigation/Navigation";
-import Products from "./Products/Products";
-import FavouriteActions from "./Products/FavouriteActions";
-import BasicButton from "./UI/BasicButton";
-import Select from "./UI/Select";
-import Loading from "./Loading/Loading";
-import Error from "./Error/Error";
+import Sidebar from "@/components/Sidebar/Sidebar";
+import Chat from "@/components/Sidebar/Chat";
+import Navigation from "@/components/Navigation/Navigation";
+import Products from "@/components/Products/Products";
+import FavouriteActions from "@/components/Products/FavouriteActions";
+import BasicButton from "@/components/UI/BasicButton";
+import Select from "@/components/UI/Select";
+import Loading from "@/components/Loading/Loading";
+import Error from "@/components/Error/Error";
 
 export {
   Sidebar,
